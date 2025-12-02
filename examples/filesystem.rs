9968f4d45521cb5258f9074741c151b600968d5c
hasezoey <hasezoey@gmail.com>
2025-12-02 18:32:06 +0100

    fix(example/filesystem::select_and_open_node): call "handle right key" instead of only opening the node
    So that it does not just show "open" but actually loads the contents.

--- examples/filesystem.rs
+++ examples/filesystem.rs
@@ -434,7 +434,7 @@ impl FileSystemTree {
 		self.component.select(MotionDirection::Upwards, idx);
 		self.component.open_all_parents(idx);
 		// always open the selected node
-		self.component.perform(Cmd::Move(Direction::Right));
+		self.handle_right_key();
 	}
 
 	/// Apply the given data as the root of the tree, resetting the state of the tree.
